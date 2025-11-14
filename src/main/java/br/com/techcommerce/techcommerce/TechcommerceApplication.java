e9c89df07a03c35573525c3325e9aea5456d5188
Willie Colatino <wcolatino@hotmail.com>
2025-11-14 10:18:22 -0300

    Criação do projeto

---
+++ src/main/java/br/com/techcommerce/techcommerce/TechcommerceApplication.java
@@ -0,0 +1,13 @@
+package br.com.techcommerce.techcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechcommerceApplication.class, args);
+	}
+
+}
